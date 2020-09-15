e56acb101c2a38f077be17a162c03c23adef246e
Wajih-alzouhairy <alzouhairy-beamro@gmail.com>
2020-09-15 09:10:32 +0200

    Updates

---
+++ precache-manifest.06408c4d9ee7ad4743f734db5de08c06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee7e596187cbf30799c4485461cfe91c",
+    "url": "/food-recipe/index.html"
+  },
+  {
+    "revision": "2384e30f729508dc3a86",
+    "url": "/food-recipe/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d28655dbb3216dd74990",
+    "url": "/food-recipe/static/js/2.2dc4c196.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-recipe/static/js/2.2dc4c196.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2384e30f729508dc3a86",
+    "url": "/food-recipe/static/js/main.b184a000.chunk.js"
+  },
+  {
+    "revision": "86678dbf4edc48bfa67c",
+    "url": "/food-recipe/static/js/runtime-main.5e435df1.js"
+  }
+]);
